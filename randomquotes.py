bfc184d0f25549c6e03f82ca33e34ddb7eb333aa
JuanDZM2105 <juandzm2105@gmail.com>
2025-05-02 16:21:33 -0500

    a

--- randomquotes.py
+++ randomquotes.py
@@ -5,13 +5,8 @@ import random
 app = Flask(__name__)
 
 phrases = [
-    "Get ready to be inspired…", 
-    "See rejection as redirection.",
-    "There is beauty in simplicity.",
-    "You can’t be late until you show up.",
-    "Maybe life is testing you. Don’t give up.",
-    "Impossible is just an opinion.",
-    "Alone or not you gonna walk forward.",
+    "La verdadera fuerza es proteger, no destruir", 
+    "La verdadera fuerza es proteger, no destruir",
 ]
 
 
